947ffdc173cf6feaa4a21b9aa7dbc626850d190c
estebaan444 <esteban.arquillo@institutvidreres.cat>
2021-11-17 12:04:59 +0100

    hola aleix

--- Ex1.java
+++ Ex1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
         public static void main (String[] args) { 
             Scanner teclat = new Scanner(System.in);
             System.out.println("Escriu un numero sencer o un 0");
+            System.out.println("Esteban Arquillo");
             int numero = teclat.nextInt();
             while (numero!=0) {
                 System.out.println("El resultat es " + numero*2);
