2f46d2351b3f9cecae0d069b606b1fd118eda67e
aleix2512 <aleix.masferrer@institutvidreres.cat>
2021-11-17 11:26:14 +0100

    Ex1 - desat

---
+++ Ex1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+    public class Ex1 {
+
+        public static void main (String[] args) { 
+            Scanner teclat = new Scanner(System.in);
+            System.out.println("Escriu un numero sencer o un 0");
+            int numero = teclat.nextInt();
+            while (numero!=0) {
+                System.out.println("El resultat es " + numero*2);
+                System.out.println("Escriu un altre numero sencer o un 0");
+                numero = teclat.nextInt();
+            }   
+        }
+    }
---
+++ Ex2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner teclat = new Scanner (System.in);
+        System.out.println("Introdueix un nombre");
+        int cont = teclat.nextInt();
+        int fact = 1;
+        int i = 1;
+        while(i<=cont)  
+        {
+            fact = fact * i;
+            i++;
+        }
+        System.out.println("El factorial de " + cont + " és " + fact );
+    }
+}    
+
+ 
+
