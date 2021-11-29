9196674e9893cfa99b0ecf29fb5809482b73c5aa
aleix2512 <aleix.masferrer@institutvidreres.cat>
2021-11-29 09:23:01 +0100

    JS_Aleix_Masferrer (Func) - desat

---
+++ JS_FUNCIONS/JS_Aleix_Masferrer_Func_Ex9.js
@@ -0,0 +1,12 @@
+var num1 = 15;
+var num2 = 15;
+
+var operacioNombres = function() {
+}
+
+if(num1+num2 == 30) {
+    console.log(true);
+}
+else if(num1 == 30 || num2 == 30) {
+    console.log(true);
+}
