d9dba056d4721d5d8c8112d7001268950475dbb9
aleix2512 <aleix.masferrer@institutvidreres.cat>
2021-12-01 23:19:15 +0100

    JS_Aleix_Masferrer_Logic_Ex2 - desat

---
+++ JS_LOGICA/JS_Aleix_Masferrer_Logic_Ex2.js
@@ -0,0 +1,8 @@
+var x = 49;
+var y = 51;
+
+x >= 10 && x <= 50 ? console.log("La variable " + x + " esta en el rang entre 10 i 50")
+                   : console.log("La variable " + x + " esta fora del rang entre 10 i 50")
+y >= 10 && y <= 50 ? console.log("La variable " + y + " esta en el rang entre 10 i 50")
+                   : console.log("La variable " + y + " esta fora del rang entre 10 i 50")      
+                   
